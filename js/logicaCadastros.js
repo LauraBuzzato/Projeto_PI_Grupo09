bf3f84f9cea2df8b20b3effe9f3d3c323477255d
MariliaToscano <marilia.araujo@sptech.school>
2025-04-22 18:13:00 -0300

    'detalhes_ultima_reuniao'

--- js/logicaCadastros.js
+++ js/logicaCadastros.js
@@ -12,13 +12,19 @@ function verificarPedido(){
 function verificarVeiculo(){
     var tipoVec = tipo.value
     var placaVec = placa.value 
-    var anoVec = Number(ano.value) 
+    var anoVec = ano.value 
 
-    if(placaVec.length > 7){
-        alert('Insira uma placa válida')
-    }else if(anoVec.length != 4){
-        alert('Insira um ano válido')
+    if(placaVec.length > 7 || placaVec.length < 5 || anoVec.length != 4 || (placaVec.length != 7 && tipoVec == 'carro' )){
+
+        if(placaVec.length > 7 || placaVec.length < 5 || (placaVec.length != 7 && tipoVec == 'carro' )){
+            alert('Insira uma placa válida')
+        }
+
+        if(anoVec.length != 4){
+            alert('Insira um ano válido')
+        }
     }else {
         alert('Veículo cadastrado com sucesso!')
     }
+ 
 }
