c6b877c8803952b16f0342dc7771fefebd465049
MariliaToscano <marilia.araujo@sptech.school>
2025-04-22 11:11:09 -0300

    logica_cadastros

--- js/logicaCadastros.js
+++ js/logicaCadastros.js
@@ -4,14 +4,21 @@ function verificarPedido(){
 
     if(documentoCliente.length > 12){
         alert('Insira um CNPJ válido')
+    }else{
+        alert('Pedido Cadastrado com sucesso!')
     }
 
 }
 function verificarVeiculo(){
     var tipoVec = tipo.value
     var placaVec = placa.value 
+    var anoVec = Number(ano.value) 
 
-    if(tipoVec.length > 7){
-
+    if(placaVec.length > 7){
+        alert('Insira uma placa válida')
+    }else if(anoVec.length != 4){
+        alert('Insira um ano válido')
+    }else {
+        alert('Veículo cadastrado com sucesso!')
     }
 }
