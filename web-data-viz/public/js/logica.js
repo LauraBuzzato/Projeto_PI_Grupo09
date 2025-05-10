1f9f5d0de39137b595a7e61a221f9b235373090e
WillianbsilvaDev <willian.dsilva@sptech.school>
2025-05-10 20:56:23 -0300

    configurando js cadastro endereço transportadora

--- web-data-viz/public/js/logica.js
+++ web-data-viz/public/js/logica.js
@@ -50,6 +50,76 @@ function cadastrarEndereco(){
         alert("preencha todos os campos!")
         return false;
     } else {
+            // Enviando o valor da nova input
+    fetch("/usuarios/cadastrar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // crie um atributo que recebe o valor recuperado aqui
+        // Agora vá para o arquivo routes/usuario.js
+        logradouroT: logradouroTransportadora,
+        cepT: cepTransportadora,
+        estadoT: estadoTransportadora,
+        cidadeT: cidadeTransportadora,
+        bairroT: bairroTransportadorabairroTransportadora,
+        numeroT: numeroTransportadora,
+        complementoT: complementoTransportadora,
+      }),
+    })
+      .then(function (resposta) {
+        console.log("resposta: ", resposta);
+
+        if (resposta.ok) {
+          cardErro.style.display = "block";
+
+          mensagem_erro.innerHTML =
+            "Cadastro realizado com sucesso! Redirecionando para tela de Login...";
+
+          setTimeout(() => {
+            window.location = "login.html";
+          }, "2000");
+
+          limparFormulario();
+          finalizarAguardar();
+        } else {
+          throw "Houve um erro ao tentar realizar o cadastro!";
+        }
+      })
+      .catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+        finalizarAguardar();
+      });
+
+    return false;
+  }
+
+  // Listando empresas cadastradas 
+  function listar() {
+    fetch("/empresas/listar", {
+      method: "GET",
+    })
+      .then(function (resposta) {
+        resposta.json().then((empresas) => {
+          empresas.forEach((empresa) => {
+            listaEmpresasCadastradas.push(empresa);
+
+            console.log("listaEmpresasCadastradas")
+            console.log(listaEmpresasCadastradas[0].codigo_ativacao)
+          });
+        });
+      })
+      .catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+      });
+  }
+
+  function sumirMensagem() {
+    cardErro.style.display = "none";
+  }
+</script>
+
         alert("Endereço da transportadora cadastrado com sucesso!")
         window.location.href = "../paginas-iniciais/login.html";
     }
