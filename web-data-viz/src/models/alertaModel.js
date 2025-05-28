fa4c5416e3220cdbc00fb93b903e9c5692cbb5b8
LauraBuzzato <laura.buzzato@sptech.school>
2025-05-28 11:50:58 -0300

    fazendo alterações lógica dashboard

--- web-data-viz/src/models/alertaModel.js
+++ web-data-viz/src/models/alertaModel.js
@@ -5,7 +5,7 @@ function  buscarDadosAlerta(idpedido){
 var instrucao = `
         select idalerta, duracao, limite, date_format(inicio, '%H:%i:%s') as inicio
         from alerta
-        where idAlerta = (select max(idAlerta) from alerta where idpedido = '${idpedido}')
+        where idalerta = (select max(idalerta) from alerta where idpedido = '${idpedido}')
     `;
     return database.executar(instrucao);
 }
